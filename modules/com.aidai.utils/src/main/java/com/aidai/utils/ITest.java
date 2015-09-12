a9beb956ed45a5ce423bdaed2b1b266cafd35f30
lq <lqjacklee@126.com>
2015-09-12 23:53:26 +0800

    添加文件

---
+++ modules/com.aidai.utils/src/main/java/com/aidai/utils/ITest.java
@@ -0,0 +1,5 @@
+package com.aidai.utils;
+
+public interface ITest {
+
+}
---
+++ modules/com.aidai.utils/src/main/java/com/aidai/utils/Test.java
@@ -0,0 +1,9 @@
+package com.aidai.utils;
+
+public class Test implements ITest{
+
+	public void init(){
+		for(int i =0;i<100;i++)
+		System.out.println("------------init-------------");
+	}
+}
